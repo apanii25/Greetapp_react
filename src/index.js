4af3a7c5520cd8c544f50a36cdd9e5d9362e8c60
apanii25 <royalanu25@gmail.com>
2023-03-04 20:05:45 +0530

    few changes in App .jsx

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css' 
-
-let curtime = new Date();
-curtime = curtime.getHours();
-let greet = '';
-const style ={};
-
-if(curtime>=1 && curtime<12){
-  greet = 'Good Morning'
-  style.color = 'green';
-
-}
-else if(curtime>=12 && curtime<=19){
-  greet = 'Good Afternooon'
-  style.color = 'orange';
-}
-else{
-  greet = 'Good Night'
-  style.color = 'blue';
-}
-
-
+import App from './App';
+import './index.css';
 ReactDOM.render(
   <>
-  <div className='head'>
-  <h1 >Hello,<span style={style}>{greet}...</span>!!!</h1>
-  </div>
-
+   <App/>
   </>,document.getElementById("root")
 );
